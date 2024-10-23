a74493e34bf11f77577991e89dc7f99d1ddb7616
Glen-Capstone <133980393+Glen-Capstone@users.noreply.github.com>
2024-10-23 00:52:34 +0000

    added some extensions

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> Reactor 123456789
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+      
       </header>
     </div>
   );
